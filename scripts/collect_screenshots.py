177f34bd907dbc515b15022550e593ab25431315
csae8092 <p.andorfer@gmail.com>
2025-03-07 09:23:10 +0100

    fix: close the browser!

--- scripts/collect_screenshots.py
+++ scripts/collect_screenshots.py
@@ -14,17 +14,18 @@ MAX_TIMEOUT = 10000
 
 print("fetching images")
 failed = []
-with sync_playwright() as p:
-    browser = p.firefox.launch()
-    for i, row in df.iterrows():
-        if row["Current availability"] == "yes":
-            url = row["URL"]
-            name = f'{row["id"]}.png'
-            f_name = os.path.join(out_dir, name)
-            if os.path.exists(f_name):
-                print(f"skipping {f_name}, already exists")
-                continue
-            print(f"saving screenshot from {url} as {f_name}")
+
+for i, row in df.iterrows():
+    if row["Current availability"] == "yes":
+        url = row["URL"]
+        name = f'{row["id"]}.png'
+        f_name = os.path.join(out_dir, name)
+        if os.path.exists(f_name):
+            print(f"skipping {f_name}, already exists")
+            continue
+        print(f"saving screenshot from {url} as {f_name}")
+        with sync_playwright() as p:
+            browser = p.firefox.launch()
             try:
                 page = browser.new_page(viewport={"width": 1200, "height": 800})
                 page.set_default_timeout(MAX_TIMEOUT)
@@ -37,17 +38,17 @@ with sync_playwright() as p:
                     "error": e
                 })
                 continue
-            with Image.open(f_name) as my_image:
-                image_height = my_image.height
-                image_width = my_image.width
-                print(
-                    "The original size of Image is: ",
-                    round(len(my_image.fp.read()) / 1024, 2),
-                    "KB",
-                )
-                my_image = my_image.resize((image_width, image_height), Image.NEAREST)
-                my_image.save(f_name)
-    browser.close()
+            browser.close()
+        with Image.open(f_name) as my_image:
+            image_height = my_image.height
+            image_width = my_image.width
+            print(
+                "The original size of Image is: ",
+                round(len(my_image.fp.read()) / 1024, 2),
+                "KB",
+            )
+            my_image = my_image.resize((image_width, image_height), Image.NEAREST)
+            my_image.save(f_name)
 df = pd.DataFrame(failed)
 df.to_csv("failed.csv", index=False)
 
